b9d1278c6bcf77f525c909fb98327988e256e63c
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-08-19 17:44:50 +0100

    added routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :bookings, only: [:index, :show, :destroy, :update] do
+    resources :reviews, only: [:create, :index. :show]
 end

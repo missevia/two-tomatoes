e5245b34816de76a1a81253c61c59b432eed8985
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-08-19 17:16:22 +0100

    Created reviews controller, added apps/views/reviews and test/controllers/reviews_controller_test.rb

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    @review.save
+    redirect_to bookings_path
+  end
+
+  private
+  def review_params
+    params.require(:review)
+end

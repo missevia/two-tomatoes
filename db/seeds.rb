14b43037b1bef6db93be9f9e1619dab3638ba91f
Moritz Indenhuck <indenhuck@posteo.de>
2019-08-20 18:22:51 +0100

    Created a seeds with users and chefs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,83 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
+puts 'Cleaning database...'
+
+Chef.destroy_all
+
+User.destroy_all
+
+puts 'Creating 20 random users...'
+
+20.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+
+  )
+end
+
+puts 'Created 20 random users.'
+
+puts 'Creating 2 random chefs with raiting 3...'
+
+2.times do
+  new_user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+  )
+  new_chef = Chef.create!(
+    name: new_user.name,
+    specialty: Faker::Restaurant.type,
+    rating: 2,
+    description: Faker::Movies::StarWars.quote,
+    user: new_user
+  )
+end
+puts 'Created 2 random chefs with raiting 3.'
+
+puts 'Creating 6 random chefs with raiting 4...'
+
+6.times do
+  new_user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+  )
+  new_chef = Chef.create!(
+    name: new_user.name,
+    specialty: Faker::Restaurant.type,
+    rating: 4,
+    description: Faker::Movies::StarWars.quote,
+    user: new_user
+  )
+end
+
+puts 'Created 6 random chefs with raiting 4.'
+
+puts 'Creating 8 random chefs with raiting 5...'
+
+8.times do
+  new_user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+  )
+  new_chef = Chef.create!(
+    name: new_user.name,
+    specialty: Faker::Restaurant.type,
+    rating: 5,
+    description: Faker::Movies::StarWars.quote,
+     user: new_user
+  )
+end
+
+puts 'Created 8 random chefs with raiting 5.'
+
+
+
+puts 'Finished!'

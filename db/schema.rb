5dcdc3653dca215d593746890790d0cadf47e0b9
Evia Horoneko <evija.horoneko@gmail.com>
2019-08-20 13:53:48 +0100

    fixed the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_20_100836) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +20,10 @@ ActiveRecord::Schema.define(version: 2019_08_20_100836) do
     t.bigint "chef_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "date"
     t.integer "number_of_people"
     t.text "description"
     t.text "preferences"
+    t.datetime "date"
     t.index ["chef_id"], name: "index_bookings_on_chef_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end

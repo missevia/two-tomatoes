270ac2b26ab641c566a74bd4e14a4e6a4fdc3b7c
Moritz Indenhuck <indenhuck@posteo.de>
2019-08-21 11:54:51 +0100

    I added image to user and updated seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_134302) do
+ActiveRecord::Schema.define(version: 2019_08_21_095042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(version: 2019_08_20_134302) do
     t.bigint "chef_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date"
     t.integer "number_of_people"
     t.text "description"
     t.text "preferences"
-    t.datetime "date"
     t.index ["chef_id"], name: "index_bookings_on_chef_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_134302) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.boolean "admin"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

e668459f1d448f2bdee68ea627026036bd6df19a
Lila Liu <lilayliu@gmail.com>
2019-08-22 12:32:55 +0100

    added simple search bar for cuisine

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,6 +4,11 @@ class ChefsController < ApplicationController
 
   def index
     @chefs = policy_scope(Chef).order(created_at: :desc)
+    if params[:query].present?
+    @results = Chef.search_by_specialty(params[:query])
+    else
+      @results = Chef.all
+    end
   end
 
   def show
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,11 @@
 class Chef < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  include PgSearch::Model
+  pg_search_scope :search_by_specialty,
+  against: [:specialty],
+  using: {
+    tsearch: { prefix: true }
+  }
 end

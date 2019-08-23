5bbecd099f1b516e19c325163b7f45f3cb952d0c
Moritz Indenhuck <indenhuck@posteo.de>
2019-08-23 15:44:05 +0100

    Fixed btn text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_142404) do
+ActiveRecord::Schema.define(version: 2019_08_23_113945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2019_08_22_142404) do
     t.boolean "admin"
     t.string "image"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
